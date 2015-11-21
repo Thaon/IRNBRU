73f9b4dfba405c5e2d3912fe1f1c50d29ed473be
kbrawley95 <kieran.brawley95@gmail.com>
2015-11-21 14:59:52 +0000

    Simple Pressure Plate

---
+++ Assets/Code/WeightTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeightTrigger : MonoBehaviour {
+
+    public GameObject start;
+    public GameObject end;
+    float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider target)
+    {
+        if (target.gameObject.tag == "Grabable")
+            transform.position = Vector3.MoveTowards(transform.position, end.transform.position, speed*Time.deltaTime);
+
+        Debug.Log("On Trap");
+    }
+
+    void OnTriggerExit(Collider target)
+    {
+        if (target.gameObject.tag == "Grabable")
+            transform.position = Vector3.MoveTowards(transform.position, start.transform.position, speed * Time.deltaTime);
+
+        Debug.Log("Off Trap");
+
+    }
+}
