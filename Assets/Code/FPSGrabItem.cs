603e898936160cde9c706d1018959ed755fef6ec
Thaon <gabriele.maddaloni@gmail.com>
2015-11-21 01:40:15 +0000

    Grabbing prototype working

--- Assets/Code/FPSGrabItem.cs
+++ Assets/Code/FPSGrabItem.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class FPSGrabItem : MonoBehaviour {
 
-    bool m_grabItem;
+    bool m_grabItemButton;
+    bool m_isCarryingItem = false;
+    GameObject m_carriedItem;
+
+    public GameObject m_grabTarget;
 
 	// Use this for initialization
 	void Start ()
@@ -13,11 +17,39 @@ public class FPSGrabItem : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        m_grabItem = Input.GetButton("Interact");
+        m_grabItemButton = Input.GetButtonDown("Interact");
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2.0f))
+        if (m_grabItemButton)
         {
-
+            if (!m_isCarryingItem)
+            {
+                //get the fucker
+                if (Physics.Raycast(transform.position, transform.forward, out hit, 5.0f))
+                {
+                    if (hit.collider.tag == "Grabable")
+                    {
+                        //Debug.Log("grabable got");
+                        m_carriedItem = hit.collider.gameObject;
+                        m_carriedItem.GetComponent<Rigidbody>().isKinematic = true;
+                        iTween.MoveTo(hit.collider.gameObject, m_grabTarget.transform.position, 1.0f);
+                        m_isCarryingItem = true;
+                    }
+                }
+            }
+            else
+            {
+                //throw the fucker
+                m_carriedItem.GetComponent<Rigidbody>().isKinematic = false;
+                m_carriedItem.GetComponent<Rigidbody>().AddForce(transform.forward * 5, ForceMode.Impulse);
+                m_isCarryingItem = false;
+                m_carriedItem = null;
+            }
+        }
+        //carry the fucker
+        if (m_carriedItem != null)
+        {
+            m_carriedItem.transform.position = m_grabTarget.transform.position;
+            m_carriedItem.transform.rotation = m_grabTarget.transform.rotation;
         }
 	}
 }
