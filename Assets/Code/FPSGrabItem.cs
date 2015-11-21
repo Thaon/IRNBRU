fe3b135dfc5ee9653c405bd04c872030691e6087
Thaon <gabriele.maddaloni@gmail.com>
2015-11-21 01:03:10 +0000

    Integrated Itween

---
+++ Assets/Code/FPSGrabItem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSGrabItem : MonoBehaviour {
+
+    bool m_grabItem;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_grabItem = Input.GetButton("Interact");
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2.0f))
+        {
+
+        }
+	}
+}
