2b08ade524b319c44c6e31e8b441e9d55b1bcc2d
kbrawley95 <kieran.brawley95@gmail.com>
2015-11-21 03:12:53 +0000

    Basic Start Menu
    With button functionality

---
+++ Assets/Code/LoadNextScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNextScene : MonoBehaviour {
+
+    public void LoadMainScene(string name)
+    {
+        Application.LoadLevel(name);
+    }
+}
