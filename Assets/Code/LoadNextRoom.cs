f2eb2091d80207323ecbc6d38dcdb45dac2f1e1b
kbrawley95 <kieran.brawley95@gmail.com>
2015-11-22 12:41:40 +0000

    Scene Loading Mechanic/Mine Prefab

---
+++ Assets/Code/LoadNextRoom.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNextRoom : MonoBehaviour {
+
+    public string name;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter(Collider target)
+    {
+        if (target.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(name);
+        }
+           
+    }
+}
